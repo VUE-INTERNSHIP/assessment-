bfe5db914f488795858c93fecd6f7c6b24337fff
Silas Bage <silasbage2018@gmail.com>
2022-09-09 08:57:32 +0100

    my first commit

---
+++ silasformvue/src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container-fluid row">
+    <div class="container-fluid  bg-info border m-3  row">
+      <div class="text-center text-white "><h1>SILAS VUE FORM</h1></div>
+      <div class="container-fluid m-3 bg-white border fm col">
+    <form class=" was-validated"  @submit="onSubmit" action="">
+      <label for="">Firstname:</label> <br>
+      <input type="text" required v-model="fname" class="form-control"> <br>
+      <Label>Lastname:</Label> <br>
+      <input type="text" required v-model="lname" class="form-control"> <br>
+      <label for="">Country:</label> <br>
+      <input type="text" required v-model="country" class="form-control"><br>
+      <label for="">Phone Number:</label> <br>
+      <input type="number" required v-model="phone" class="form-control"> <br>
+      <label for="">Age:</label> <br>
+      <input type="number" required v-model="age" class="form-control"> <br>
+      <label for="">Email Address:</label> <br>
+      <input type="link" required v-model="email" class="form-control">
+      <div><button class="btn mt-2 btn-primary border" v:>Submit</button></div>
+    </form>
+    </div> <br>
+    
+    <div v-for="(field, x) in data" :key="x" class="output border text-info col bg-white m-3 text-bold container-fluid">
+      <div class="row ">
+      <h4 class="col">Firstname:</h4> 
+      <p class="col text-dark"><b>{{field.fname}}</b></p>
+      </div>
+      <div class="row">
+      <h4 class="col">Lastname:</h4>  
+      <p class="col text-dark"><b>{{field.lname}}</b></p>
+      </div>
+      <div class="row">
+      <h4 class="col">Country:</h4> 
+      <p class="col text-dark"><b>{{field.country}}</b></p>
+     </div>
+     <div class="row">
+      <h4 class="col">Phone Number:</h4> 
+      <p class="col text-dark"><b>{{field.phone}}</b></p>
+     </div>
+     <div class="row">
+      <h4 class="col">Email Address:</h4> 
+      <p class="col text-dark"><b>{{field.email}}</b></p>
+     </div>
+    </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    name: 'app',
+    data(){
+      return{
+        output:[
+
+        ],
+        fname: "",
+        lname: "",
+        country: "",
+        phone: "",
+        age: "",
+        email: "",
+      }
+    },
+    methods: {
+      onSubmit(text){
+        text.preventDefault()
+        const fm = {
+          fname: this.fname,
+          lname: this.lname,
+          country: this.country,
+          phone: this.phone,
+          age: this.age,
+          email: this.email,
+      }
+      this.data ={fm}
+      this.fname = "",
+      this.lname = "",
+      this.country= "",
+      this.phone= "",
+      this.age= "",
+      this.email= ""
+    }
+  }
+}
+</script>
+<style scoped>
+  .output{
+  
+  }
+  .fm{
+    
+  }
+</style>
---
+++ silasrouter/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <!--<div class="justify-content-center container-fluid">
+      <ul class=" nav nav-pills nav-tabs justify-content-center p-4 container-fluid border">
+      <li class="nav-item col p-4"><router-link to="/">Home</router-link></li>
+      <li class="nav-item col p-4"><router-link to="/about">About</router-link></li>
+      <li class="nav-item col p-4"><router-link to="/contact">Contact Us</router-link></li>
+    </ul>
+  </div>-->
+
+  <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><b>GladCoTECH</b></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link active" aria-current="page" >Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link" >About</router-link>
+        </li>
+        <li>
+          <router-link to="/contact" class="nav-link" >Contact Us</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img id="img1" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img id="img2" class="d-block w-100" alt="...">
+    </div><div class="bg-text">
+       <h3 class="text-blue-200 tracking-wide text-2xl font-bold"><b>GladCoTECH</b></h3>
+      <h1 class="text-5xl font-bold tracking-widest mb-3 mt-3">TECH AT ITS PEAK</h1>
+      <button class="btn btn-outline rounded-pill border-white font-bold text-xl text-white mt-5 hover:bg-blue-500">Our Products</button> 
+      </div>
+    
+    <div class="carousel-item">
+      <img id="img3" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img id="img4" class="d-block w-100" alt="...">
+    </div>
+    
+  </div>
+</div>
+    <router-view/>
+    <footer class="jumbotron"> <div class="text-center container-fluid jumbotron">
+      © GladCoTECH.com, Inc. All rights reserved.
+    </div></footer>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  .nav-item{
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+img {
+  width: 100%;
+  height: 50%;
+}
+</style>
