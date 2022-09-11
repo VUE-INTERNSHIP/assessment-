b24e5e15606fdafaf5a167812350ebcc138228bd
itszona <davekatchy@gmail.com>
2022-09-11 23:25:07 +0100

    router vue app

---
+++ src/views/AboutView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="bg">
+      <div class="text">
+        <h1>Best Place To Learn Graphic Design!</h1>
+        <button class="btn btn-secondary rounded">Join us now!</button>
+      </div>
+    </div>
+    
+    <div>
+      <h1 class="text-center mt-3 fs-1 fw-bold">About</h1>
+      <div class="row align-items-end py-5 m-5">
+        <div class="col-lg-4 col-md-6">
+          <h3 class="text-center">Technology</h3>
+          <p class="text-center text-secondary mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut incidunt qui modi voluptatibus quisquam laboriosam ea voluptatum eos nesciunt unde, recusandae vitae molestias dolore debitis eligendi ratione at quos ex.</p>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <h3 class="text-center">Creative Ideas</h3>
+          <p class="text-center text-secondary mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut incidunt qui modi voluptatibus quisquam laboriosam ea voluptatum eos nesciunt unde, recusandae vitae molestias dolore debitis eligendi ratione at quos ex.</p>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <h3 class="text-center">Useful Tricks</h3>
+          <p class="text-center text-secondary mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut incidunt qui modi voluptatibus quisquam laboriosam ea voluptatum eos nesciunt unde, recusandae vitae molestias dolore debitis eligendi ratione at quos ex.</p>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <h3 class="text-center">Ready Target</h3>
+          <p class="text-center text-secondary mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut incidunt qui modi voluptatibus quisquam laboriosam ea voluptatum eos nesciunt unde, recusandae vitae molestias dolore debitis eligendi ratione at quos ex.</p>
+        </div>
+      </div>
+    </div> 
+
+    <footer class="page-footer font-small blue bg-secondary"
+  >
+      <div class="footer-copyright text-center py-3">© David 2022 
+      </div>
+    </footer>
+
+  
+  </div>
+
+</template>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="bg">
+            <div class="text">
+                <h1>Best Place To Learn Graphic Design!</h1>
+                <button class="btn btn-secondary rounded">Join us now!</button>
+            </div>
+        </div>
+
+        <h1 class="text-center fw-bold mt-3 fs-1">Contact</h1>
+        <div>
+            <h3 class="text-center fst-italic mt-3">If you have any query, please contact us</h3>
+            <p class="text-center m-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque magni magnam illo excepturi, nobis asperiores inventore esse enim ratione tempora. Maiores aut itaque beatae. Molestias, tempore! Magnam aliquid in officiis.</p>
+            <div class="form justify-content-center container-fluid">    
+                <form class="row g-3 container-fluid py-5 justify-content-center">
+                <div class="col-12">
+                    <label for="inputName" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="inputName" placeholder="John Doe">
+                </div>
+                <div class="col-md-6">
+                    <label for="inputEmail4" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4">
+                </div>
+                <div class="col-md-6">
+                    <label for="inputSubject" class="form-label">Subject</label>
+                    <input type="text" class="form-control" id="inputSubject">
+                </div>
+                <div class="col-12">
+                    <label for="inputMessage" class="form-label">Message</label>
+                    <textarea class="form-control" placeholder="Leave a message here" id="floatingTextarea2" style="height: 100px"></textarea>
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    
+        <footer class="page-footer font-small blue bg-secondary"
+  >
+            <div class="footer-copyright text-center py-3">© David 2022 
+            </div>
+        </footer>
+
+    
+    </div>
+    
+    
+</template>
